045f708f16753880c7b6e8c345f56fde163d467c
Kamsi Ozorji <ozorjikj@gmail.com>
2024-02-25 20:34:38 -0600

    first changes

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
+        <time>{YEAR}</time> © Kamsi Ozorji.
         <a href="/feed.xml">RSS</a>
       </small>
       <style jsx>{`
